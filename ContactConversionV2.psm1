fb6eb5892b509cf16af2f43a9d7442b65e59716e
TIMMCMIC <timmcmic@microsoft.com>
2022-10-02 10:25:32 -0400

    Updating

--- ContactConversionV2.psm1
+++ ContactConversionV2.psm1
@@ -316,9 +316,9 @@ Function Start-ContactMigration
 
     #>
 
-    Import-Module DLConversionV2 -PassThru
+    Import-Module DLConversionV2 -PassThru -Force
 
-    Get-ADObjectConfiguration -groupSMTPAddress "tim@e-mcmichael.com" -globalCatalogServer "azure-dc-0.home.e-mcmichael.com" -parameterSet '*' -errorAction STOP -adCredential (get-credential)
+    & {Get-ADObjectConfiguration -groupSMTPAddress "tim@e-mcmichael.com" -globalCatalogServer "azure-dc-0.home.e-mcmichael.com" -parameterSet '*' -errorAction STOP -adCredential (get-credential)}
 
     <#
 
